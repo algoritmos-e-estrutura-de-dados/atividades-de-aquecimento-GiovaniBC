200d09db901ec9a3aaf80a629448e2b2c9e922e9
Laboratório Informática <giovani.correa@edu.unifil.br>
2022-08-15 22:10:31 -0300

    exercicios

---
+++ Exercicios/Average2.py
@@ -0,0 +1,7 @@
+a = input()
+b = input()
+c = input()
+
+media = (float(a) * 2 + float(b) * 3 + float(c) * 5)/10
+
+print (f"MEDIA = {media: .1f}")
---
+++ Exercicios/salary.py
@@ -0,0 +1,8 @@
+num = input("Informe seu número: ")
+hpm = input("Informe seu total de horas trabalhadas no mês: ")
+dph = input("Informe a quantidade ganha por hora trabalhada: ")
+
+salary = float(hpm) * float(dph)
+
+print(f"NUMBER = {num}")
+print(f"SALARY = U$ {salary}")
